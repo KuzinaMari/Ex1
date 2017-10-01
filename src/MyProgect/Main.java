72b31ffff6123ccf5bf0a783bd2b09467c81da9a
KuzinaMari <Novamaria98@mail.ru>
2017-10-01 14:03:27 +0300

    Update Main.java

--- src/MyProgect/Main.java
+++ src/MyProgect/Main.java
@@ -1,43 +1,48 @@
 package MyProgect;
 
 public class Main {
-    public static int[] P = {11, 12, 3, 7, 33, 2, 41, 1};
+    private static int[] P ;
 
     public static void main(String[] args) {
 
-        System.out.println(fun(7));
+        Result res = findMaxSub( P );
+        System.out.println( res.toString() );
 
     }
 
-    public static Prev fun(int imax) {
-        if (imax == 0) {
-            throw new RuntimeException("incorrect index");
-        }
-        Prev res = new Prev();
-        if (imax == 1) {
-            int p0 = P[0];
-            int p1 = P[1];
-            int minIndex = p0 < p1 ? 0 : 1;
-            res.minI = minIndex;
-            res.minP = P[minIndex];
-            res.min = p0;
-            res.max = p1;
-            res.delta = p1 - p0;
-            return res;
-        }
-        Prev prev = fun(imax - 1);
-        int minIndex2 = P[imax] < prev.minP ? minIndex2 = imax : prev.minI;
-        int min = P[imax] - prev.minP > prev.delta ? prev.minP : prev.min;
-        int max = P[imax] - prev.minP > prev.delta ? P[imax] : prev.max;
-        int maxIndex2= (P[imax] > prev.max)&&(P[imax] - prev.minP > prev.delta) ? imax : prev.maxInd;
-        int minIndex3= (P[imax] < prev.minP)&&(P[imax] - prev.minP > prev.delta) ? imax : prev.minI;
-        res.minI = minIndex2;
-        res.minP = P[minIndex2];
-        res.maxInd = maxIndex2;
-        res.minInd = minIndex3;
-        res.min = min;
-        res.max = max;
-        res.delta = max - min;
-        return res;
+    public static void setP(int[] p) {
+        P = p;
     }
+
+    public static Result findMaxSub(int[] prices ){
+        P = prices;
+        int p0 = P[0];
+        int p1 = P[1];
+        int minI = p0 < p1 ? 0 : 1;
+        int minP = prices[ minI ]; //минимум во всём массиве
+        int minInd = 0;
+        int maxInd = 1;
+        int min = p0;
+        int max = p1;
+        int delta = p1 - p0;
+
+        for( int imax = 2; imax < prices.length; imax++ ){
+            int last = prices[ imax ];
+            if( last - minP > delta ){ //если подмассив с последним элементом больше текущего
+                min = minP; //минимум теперь - это минимум во всём массиве
+                max = last; //а максимум - просто последний, иначе бы максимальным
+                  //подмассивом был бы просто максимальный в предыдущем [0..imax-1]
+                minInd = minI;
+                maxInd = imax;
+            }
+            delta = max - min;
+            if( last < minP ){ //это одновременно мы ищем минимальный элемент
+                minP = last;
+                minI = imax;
+            }
+        }
+
+        return new Result( minI, maxInd, minInd, minP, min, max, delta );
+    }
+
 }
