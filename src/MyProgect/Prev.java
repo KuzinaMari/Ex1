42458ab3daaf65c36c90bb0ee7855ebbccdc261d
KuzinaMari <Novamaria98@mail.ru>
2017-10-01 14:04:01 +0300

    Update Prev.java

--- src/MyProgect/Prev.java
+++ src/MyProgect/Prev.java
@@ -3,17 +3,52 @@ package MyProgect;
 /**
  * Created by 1 on 15.09.2017.
  */
-public class Prev {
-    int minI;
-    int maxInd;
-    int minInd;
-    int minP;
-    int min;
-    int max;
-    int delta;
+public class Result {
+    private int minI;
+    private int maxInd;
+    private int minInd;
+    private int minP;
 
-    public Prev() {
+    public int getMinI() {
+        return minI;
+    }
 
+    public int getMaxInd() {
+        return maxInd;
+    }
+
+    public int getMinInd() {
+        return minInd;
+    }
+
+    public int getMinP() {
+        return minP;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public int getDelta() {
+        return delta;
+    }
+
+    private int min;
+    private int max;
+    private int delta;
+
+    public Result(int minI, int maxInd, int minInd, int minP, int min, int max, int delta) {
+        this.minI = minI;
+        this.maxInd = maxInd;
+        this.minInd = minInd;
+        this.minP = minP;
+        this.min = min;
+        this.max = max;
+        this.delta = delta;
     }
 
     public String toString() {
